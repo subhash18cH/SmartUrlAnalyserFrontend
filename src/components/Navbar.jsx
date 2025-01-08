3980766690583006609b80dc306487b0dfdf3ed6
subhash18cH <subhashcnd15@gmail.com>
2025-01-08 23:10:25 +0530

    added responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,13 +8,13 @@ const Navbar = () => {
       <nav className='bg-white shadow-lg fixed w-full  '>
         <div className='flex justify-between items-center p-3'>
           <div className='flex justify-center items-center'>
-            <img className='rounded-full w-24' src={logo} alt="Logo" />
-            <h1 className='font-semibold text-2xl'>SmartUrl</h1>
+            <img className='rounded-full lg:w-24 md:w-24 w-14' src={logo} alt="Logo" />
+            <h1 className='font-semibold md:text-2xl lg:text-2xl text-xl'>SmartUrl</h1>
           </div>
 
-          <div className='space-x-8'>
+          <div className='lg:space-x-8'>
 
-            <NavLink to={"/signin"} className='py-2 px-4 font-semibold rounded-lg text-gray-800 hover:text-black'>Log In</NavLink>
+            <NavLink to={"/signin"} className='py-2 px-4 font-semibold rounded-lg text-gray-800 hover:text-black '>Log In</NavLink>
             <NavLink to={"/signup"} className=' outline-none py-3 px-4 border bg-blue-700 text-white hover:bg-blue-600 font-semibold rounded-md'>Sign Up</NavLink>
           </div>
         </div>
